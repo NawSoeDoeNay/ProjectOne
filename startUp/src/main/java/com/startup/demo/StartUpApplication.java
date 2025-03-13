65857a47393966c4e5a8eaa9e37958d4a7a19ff0
NawSoeDoeNay <nawsoedoenay@gmail.com>
2025-03-13 20:36:17 +0630

    test

---
+++ startUp/src/main/java/com/startup/demo/StartUpApplication.java
@@ -0,0 +1,15 @@
+package com.startup.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartUpApplication.class, args);
+	}
+	
+	
+
+}
---
+++ startUp/src/test/java/com/startup/demo/StartUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.startup.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
